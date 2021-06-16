117030440d026b9a7974d030c32ee0e8635dfc28
SilverFox197 <lit20009@byui.edu>
2021-06-15 20:45:39 -0400

    Ninja

--- Raisemewell/app/src/main/java/com/example/raisemewell/MainActivity.java
+++ Raisemewell/app/src/main/java/com/example/raisemewell/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.raisemewell;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,6 +13,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // hello Ninja
     }
 }
