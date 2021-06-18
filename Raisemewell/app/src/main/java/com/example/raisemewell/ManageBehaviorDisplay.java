3a3988d4bf7ab98406a6b6cca01834a451032db1
SilverFox197 <lit20009@byui.edu>
2021-06-17 20:11:54 -0400

    Manage Behavior Display

---
+++ Raisemewell/app/src/main/java/com/example/raisemewell/ManageBehaviorDisplay.java
@@ -0,0 +1,14 @@
+package com.example.raisemewell;
+
+import java.util.List;
+
+public class ManageBehaviorDisplay {
+    public goodBehavior () {
+        println.("Good Behavoir");
+        println.();
+    }
+    public badBehavior () {
+        println.("Bad Behavior");
+        println.();
+    }
+}
