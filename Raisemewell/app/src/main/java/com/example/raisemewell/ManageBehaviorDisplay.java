65bbd3467b9f1af840b7030e2e4a3517e0d148b6
Bradey Merrill <bradage.merrill@gmail.com>
2021-07-06 18:41:44 -0600

    Created Add Child Display

--- Raisemewell/app/src/main/java/com/example/raisemewell/ManageBehaviorDisplay.java
+++ Raisemewell/app/src/main/java/com/example/raisemewell/ManageBehaviorDisplay.java
@@ -1,6 +1,7 @@
 package com.example.raisemewell;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,44 +22,49 @@ public class ManageBehaviorDisplay extends AppCompatActivity {
         setContentView(R.layout.activity_manage_behavior_display);
     }
 
-    public void addBehavior(View v){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Add New Behavior");
-        // Set up the input
-        final EditText behaviorInput = new EditText(this);
-// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-        behaviorInput.setHint("Enter Behavior");
-        behaviorInput.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
-        builder.setView(behaviorInput);
-        builder.setMessage("Enter BehaviorWeight");
-        final EditText behaviorWeightInput = new EditText(this);
-// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-        behaviorWeightInput.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_TEXT_VARIATION_NORMAL);
-        builder.setView(behaviorWeightInput);
-//        builder.setMessage("Enter BehaviorType");
-//        EditText behaviorTypeInput = new EditText(this);
-//// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-//        behaviorTypeInput.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_TEXT_VARIATION_NORMAL);
-//        builder.setView(behaviorTypeInput);
-
-// Set up the buttons
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-               _behavior = behaviorInput.getText().toString();
-                _behaviorWeight = Integer.parseInt(behaviorWeightInput.getText().toString());
-//               _behaviorType = behaviorTypeInput.getText().toString();
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-
-        builder.show();
+    /**Called when the user taps the + button */
+    public void addBehavior(View view) {
+        Intent intent = new Intent(this, AddBehaviorDisplay.class);
+        startActivity(intent);
     }
+//    public void addBehavior(View v){
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle("Add New Behavior");
+//        // Set up the input
+//        final EditText behaviorInput = new EditText(this);
+//// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+//        behaviorInput.setHint("Enter Behavior");
+//        behaviorInput.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+//        builder.setView(behaviorInput);
+//        builder.setMessage("Enter BehaviorWeight");
+//        final EditText behaviorWeightInput = new EditText(this);
+//// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+//        behaviorWeightInput.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_TEXT_VARIATION_NORMAL);
+//        builder.setView(behaviorWeightInput);
+////        builder.setMessage("Enter BehaviorType");
+////        EditText behaviorTypeInput = new EditText(this);
+////// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+////        behaviorTypeInput.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_TEXT_VARIATION_NORMAL);
+////        builder.setView(behaviorTypeInput);
+//
+//// Set up the buttons
+//        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//               _behavior = behaviorInput.getText().toString();
+//                _behaviorWeight = Integer.parseInt(behaviorWeightInput.getText().toString());
+////               _behaviorType = behaviorTypeInput.getText().toString();
+//            }
+//        });
+//        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                dialog.cancel();
+//            }
+//        });
+//
+//        builder.show();
+//    }
 
     //    public goodBehavior () {
 //        println.("Good Behavoir");
